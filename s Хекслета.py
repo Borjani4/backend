56fb18187b8322b4f1336842f03a05f4c80cdfe8
Borjanrus83 <74932759+Borjanrus83@users.noreply.github.com>
2022-04-08 16:09:49 +0300

    made Хекслет.py

---
+++ s Хекслета.py
@@ -0,0 +1,13 @@
+"""print("Gregor Cleagane\nDunsen\nPolliver\nChiswyke")"""
+
+
+print("Gregor \"Cleagane\"")
+print(' ')
+print ("Joffre loves using \\n")
+print("Dragon 'stone' jeffry")
+print('Kings'+'wood'+'Kings'+'road'+'Kings'+'road'+  str(122538))
+print("computer")
+print(" King's "  +  'Landinge')
+print ("Who's to blame?")
+print("\\")
+
--- task05.py
+++ task05.py
@@ -4,3 +4,10 @@ print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
 print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
 
 
+"""Задача «Следующее и предыдущее»
+Условие
+Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!)."""
+
+# int "12" -> 12
+# str 12 -> "12"
+# "hello " + "harry"
