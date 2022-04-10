acc7558747d2fd251250115befbe1374bb6d9ddc
Borjanrus83 <74932759+Borjanrus83@users.noreply.github.com>
2022-04-11 01:08:18 +0300

    made task04.py

--- task04.py
+++ task04.py
@@ -3,4 +3,14 @@ k=int(input())
 print(k/n)
 print(k%n)"""
 
-print('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')
+"""print("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".")
+
+- Did Joffrey agree?
+- He did. He also said "I love using \n"."""
+
+family = 'Targaryen'
+pet = 'Dragon' 
+
+print(family)
+print(' and ')
+print(pet)
