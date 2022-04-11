8ab4ec23a3d1c7ae2ef0bd882df786f875640e7e
Borjanrus83 <74932759+Borjanrus83@users.noreply.github.com>
2022-04-12 01:07:26 +0300

    made king.py

---
+++ king.py
@@ -0,0 +1,7 @@
+king = 'King Balon the 6th'
+RoomsDynastyKing=17
+DynastyKing=6
+print(king + ' has ' + str(6 * 17) + ' rooms.')
+
+potomstvo = 6 * 17
+print(king + ' has ' + str(potomstvo) + " rooms.")
